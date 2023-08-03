575d5c190e0f12b1c40edba9afa9fe834dca6646
Iamneo <techadmin@iamneo.ai>
2023-08-03 14:42:18 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/RunWith.java
+++ springapp/src/test/java/com/examly/springapp/RunWith.java
@@ -1,3 +1,9 @@
-public class RunWith {
-    
+package com.examly.springapp;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+public @interface RunWith {
+
+    Class<SpringRunner> value();
+
 }
